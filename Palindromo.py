1f34fdde46714d6fc068bb96d231520515c069da
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-17 17:35:49 +0100

    modificación añadiendo el main.py

--- Palindromo.py
+++ Palindromo.py
@@ -13,5 +13,18 @@ class Palindromo:
             print(True)
         else:
             print(False)
-    contenido = str(input("Introduzca una frase/palabra/número: "))
-    esPalindromo(contenido)
+        
+
+    def test():
+        a, b = 'áéíóúüñÁÉÍÓÚÜ', 'aeiouunAEIOUU'
+        tilde = str.maketrans(a, b)
+        contenido = contenido.lower()  # Convierto el texto en minúsculas.
+        contenido = contenido.replace(' ', '')  # Quito los espacios.
+        contenido = contenido.translate(tilde)  # Elimino las tildes.
+        lista = list(contenido)  # Convierto en el contenido en una lista.
+        listaresultado = list(reversed(contenido))  # Invierto la lista.
+        if lista == listaresultado:  # Comparo el contenido original con el inverso.
+            print(True)
+        else:
+            print(False)
+           
---
+++ main.py
@@ -0,0 +1,3 @@
+from Palindromo import *
+p = Palindromo
+print(p.esPalindromo(str(input("Introduzca una frase/palabra/número para comprobar si es palíndromo: "))))
