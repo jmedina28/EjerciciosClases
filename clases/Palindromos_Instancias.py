ce4436e7ccff3f6d6ad5c77cc8b4be539b8dcb12
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-17 19:30:54 +0100

    prueba1

--- clases/Palindromos_Instancias.py
+++ clases/Palindromos_Instancias.py
@@ -1,21 +1,41 @@
 # Palíndromos
+from operator import truediv
 import os
 
-class Palindromo:
+class Palindromos():
     def __init__(self, atributo):
         self.atributo = atributo
-        self.doct = open("Palindromos.txt", "w")
 
-    def test(self):
-            self.doct.write(str(self.atributo)+os.linesep)
+    def test(self, contenido):
+            self.doct = open("Palindromos.txt", "w")
+            ultima_linea = self.doct.readlines()[-1]
+            if Palindromos().test(str(ultima_linea)) == True:
+                print(str(ultima_linea))
+            self.mayuscula = contenido.upper()
+            self.doct.write(str(self.mayuscula)+os.linesep)
+            self.doct.close()
             a, b = 'áéíóúüñÁÉÍÓÚÜ', 'aeiouunAEIOUU'
-            tilde = str.maketrans(a, b)
-            self.atributo = self.atributo.lower()  # Convierto el texto en minúsculas.
-            self.atributo = self.atributo.replace(' ', '')  # Quito los espacios.
-            self.atributo = self.atributo.translate(tilde)  # Elimino las tildes.
-            self.lista = list(self.atributo)  # Convierto el atributo en una lista.
-            self.listaresultado = list(reversed(self.atributo))  # Invierto la lista.
+            self.tilde = str.maketrans(a, b)
+            contenido = contenido.lower()  # Convierto el texto en minúsculas.
+            contenido = contenido.replace(' ', '')  # Quito los espacios.
+            contenido = contenido.translate(self.tilde)  # Elimino las tildes.
+            self.lista = list(contenido)  # Convierto el atributo en una lista.
+            self.listaresultado = list(reversed(contenido))  # Invierto la lista.
             if self.lista == self.listaresultado:  # Comparo el atributo original con el inverso.
                 print(True)
+                return True
             else:
                 print(False)
+                return False
+
+    def destructor(self):
+        self.doct = open("Palindromos.txt", "r")
+        ultima_linea = self.doct.readlines()[-1]
+        if Palindromos().test(self.atributo) == False:
+            print(str(ultima_linea))
+        self.doct.close()
+
+
+    def ejecutar(self):
+        Palindromos().test(self.atributo)
+        Palindromos().destructor()
