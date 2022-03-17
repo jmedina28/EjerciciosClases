58b2856e69c8861ce88e0dab5eb8d10ba26dca83
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-17 17:59:48 +0100

    cambios estéticos de cara a la ejecución

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 from Palindromo import *
-p = Palindromo
-print(p.esPalindromo(str(input(
-    "Introduzca una frase/palabra/número para comprobar si es palíndromo: "))))
+def seleccionejercicio(variable):
+
+    if variable == 1:
+        p = Palindromo
+        print(p.esPalindromo(str(input(
+            "Introduzca una frase/palabra/número para comprobar si es palíndromo: "))))
+    elif variable == 2:
+        print("En desarrollo...")
+    elif variable == 3:
+        print("En desarrollo...")
+    elif variable == 4:
+        print("En desarrollo...")
+    else:
+        print("Introduzca valores correctos por favor.")
+        seleccionejercicio(int(input("""
+1 - Palíndromo(Método de clase).
+2 - Palíndromo(Método de instancia).
+3 - Puzzle.
+4 - Logger.
+
+Seleccione que ejercicio desea ejecutar(1-4): """)))
+
+variable = int(input("""
+1 - Palíndromo(Método de clase).
+2 - Palíndromo(Método de instancia).
+3 - Puzzle.
+4 - Logger.
+
+Seleccione que ejercicio desea ejecutar(1-4): """))
+
+seleccionejercicio(variable)
