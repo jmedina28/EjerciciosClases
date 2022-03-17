f78bf93ac07ec6a59792d85ebd64ad5d2712b114
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-17 17:43:11 +0100

    modificación estética

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from Palindromo import *
 p = Palindromo
-print(p.esPalindromo(str(input("Introduzca una frase/palabra/número para comprobar si es palíndromo: "))))
+print(p.esPalindromo(str(input(
+    "Introduzca una frase/palabra/número para comprobar si es palíndromo: "))))
