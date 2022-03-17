3d4309bc22322f417609f92330bf797ee04ac251
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-17 16:58:35 +0100

    pep8

--- Palindromo.py
+++ Palindromo.py
@@ -1,15 +1,14 @@
 # Palíndromos
-
 class Palindromo():
     def palindromo(contenido):
-        a,b = 'áéíóúüñÁÉÍÓÚÜ','aeiouunAEIOUU'
-        tilde = str.maketrans(a,b)
-        contenido = contenido.lower() # Convierto el texto en minúsculas.
-        contenido = contenido.replace(' ', '') # Quito los espacios.
-        contenido = contenido.translate(tilde) # Elimino las tildes.
-        lista = list(contenido) # Convierto en el contenido en una lista.
-        listaresultado = list(reversed(contenido)) # Invierto la lista.
-        if lista == listaresultado: # Comparo el contenido original con el inverso.
+        a, b = 'áéíóúüñÁÉÍÓÚÜ', 'aeiouunAEIOUU'
+        tilde = str.maketrans(a, b)
+        contenido = contenido.lower()  # Convierto el texto en minúsculas.
+        contenido = contenido.replace(' ', '')  # Quito los espacios.
+        contenido = contenido.translate(tilde)  # Elimino las tildes.
+        lista = list(contenido)  # Convierto en el contenido en una lista.
+        listaresultado = list(reversed(contenido))  # Invierto la lista.
+        if lista == listaresultado:  # Comparo el contenido original con el inverso.
             print("Esta frase/palabra/número es un palíndromo.")
         else:
             print("No es palíndromo.")
