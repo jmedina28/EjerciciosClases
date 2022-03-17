15068e615186c018897a5b9db26159b7c4a7f707
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-17 20:43:19 +0100

    cambios varios

--- clases/Palindromo.py
+++ clases/Palindromo.py
@@ -1,13 +1,14 @@
 # Palíndromos
 
 class Palindromo:
+
     def esPalindromo(contenido):
         a, b = 'áéíóúüñÁÉÍÓÚÜ', 'aeiouunAEIOUU'
         tilde = str.maketrans(a, b)
         contenido = contenido.lower()  # Convierto el texto en minúsculas.
         contenido = contenido.replace(' ', '')  # Quito los espacios.
         contenido = contenido.translate(tilde)  # Elimino las tildes.
-        lista = list(contenido)  # Convierto en el contenido en una lista.
+        lista = list(contenido)  # Convierto el contenido en una lista.
         listaresultado = list(reversed(contenido))  # Invierto la lista.
         if lista == listaresultado:  # Comparo el contenido original con el inverso.
             print(True)
@@ -15,17 +16,3 @@ class Palindromo:
         else:
             print(False)
             exit()
-        
-    def test():
-        a, b = 'áéíóúüñÁÉÍÓÚÜ', 'aeiouunAEIOUU'
-        tilde = str.maketrans(a, b)
-        contenido = contenido.lower()  # Convierto el texto en minúsculas.
-        contenido = contenido.replace(' ', '')  # Quito los espacios.
-        contenido = contenido.translate(tilde)  # Elimino las tildes.
-        lista = list(contenido)  # Convierto en el contenido en una lista.
-        listaresultado = list(reversed(contenido))  # Invierto la lista.
-        if lista == listaresultado:  # Comparo el contenido original con el inverso.
-            print(True)
-        else:
-            print(False)
-           
